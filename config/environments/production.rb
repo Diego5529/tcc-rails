c5433de2683f127881bd8a47ce95bfac032abd9e
Diego5529 <diego.6.souza@gmail.com>
2015-09-27 20:06:57 -0300

    inital commit with heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,16 +78,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.default_url_options = { :host => 'http://yoursite.herokuapp.com' }
-
-config.action_mailer.delivery_method = :smtp
-
-config.action_mailer.smtp_settings = {
-address: "smtp.gmail.com",
-port: 587,
-domain: ENV["GMAIL_DOMAIN"],
-authentication: "plain",
-enable_starttls_auto: true,
-user_name: ENV["GMAIL_USERNAME"],
-password: ENV["GMAIL_PASSWORD"]
-}
 end
