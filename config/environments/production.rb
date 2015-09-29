ab039326357541383da2a662d27e4431a67d6eec
Diego5529 <diego.6.souza@gmail.com>
2015-09-29 20:21:30 -0300

    deploy mailer 2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,10 +85,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
     user_name: 'footwear.clickjogos@gmail.com',
     password: 'clickjogos',
-    authentication: 'plain',
+    authentication: :plain,
     enable_starttls_auto: true,
     :openssl_verify_mode => 'none'  # This line added and it works fine
 }
