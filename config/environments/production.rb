585ebae23bfa8cbf49775eefb1bd0ca1b6d70d3a
Diego5529 <diego.6.souza@gmail.com>
2015-09-29 20:10:01 -0300

    deploy mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,17 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
+  config.action_mailer.default_url_options = { :host => 'tcc-rails.herokuapp.com' }
+  config.action_mailer.perform_deliveries = true
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'http://tcc-rails.herokuapp.com' }
 
   config.action_mailer.smtp_settings = {
-    :address        => "smtp.gmail.com",
-    :port           => 587,
-    :authentication => :plain,
-    :user_name      => "footwear.clickjogos@gmail.com",
-    :password       => "clickjogos"
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :user_name            => 'footwear.clickjogos@gmail.com',
+    :password             => 'clickjogos',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
   }
 end
