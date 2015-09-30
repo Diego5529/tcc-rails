34aa76482caee33d86b2becd692776277fcc95c5
Diego5529 <diego.6.souza@gmail.com>
2015-09-30 20:10:20 -0300

    teste heroku send email

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,11 +38,12 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-  config.action_mailer.perform_deliveries = true # Set it to false to disable the email in dev mode
-  config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+
+  ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           => 587,
     :authentication => :plain,
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,14 +22,14 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  #config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
+  config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -77,19 +77,18 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'https://tcc-rails.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'http://tcc-rails.herokuapp.com' }
 
-  config.action_mailer.perform_deliveries = true
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :smtp
-
-  config.action_mailer.smtp_settings = {
+  ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :domain               => 'heroku.com',
     :user_name            => 'footwear.clickjogos@gmail.com',
     :password             => 'clickjogos',
-    :authentication       => :plain,
+    :authentication       => "plain",
     :enable_starttls_auto => true
   }
 end
