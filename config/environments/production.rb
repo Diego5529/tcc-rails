1c75bd3d847fe4b53efbc67113d1f5673e4f1937
Diego5529 <diego.6.souza@gmail.com>
2015-10-02 20:34:36 -0300

    teste send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,12 +84,12 @@ Rails.application.configure do
   config.action_mailer.default :charset => "utf-8"
 
   ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.sendgrid.net',
-    :port                 => '2525',
-    :domain               => 'heroku.com',
-    :user_name            => 'Diego5529',
-    :password             => 'F4cebooklk',
-    :authentication       => "plain",
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
 
