eee06564512fbebe0257da04a0f8e8c6839f1ab1
Diego5529 <diego.6.souza@gmail.com>
2015-10-04 17:56:59 -0300

    tteste gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,8 +79,6 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'http://tcc-rails.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
 
   # ActionMailer::Base.smtp_settings = {
   #   :address        => 'smtp.sendgrid.net',
@@ -96,8 +94,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => ENV['gmail_username'],
-    :password             => ENV['gmail_password'],
+    :user_name            => 'footwear.clickjogos@gmail.com'],
+    :password             => 'clickjogostrovata',
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
