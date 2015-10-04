f2eba0f2bc1b4e4926b22886491f7074ae72091a
Diego5529 <diego.6.souza@gmail.com>
2015-10-04 19:25:40 -0300

    teste again...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,8 +94,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
-    :user_name            => 'footwear.clickjogos@gmail.com',
-    :password             => 'clickjogostrovata'
+    :user_name            => ENV['gmail_username'],
+    :password             => ENV['gmail_password']
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
