3fe6cb760310366caa7fb55bb9c339f7685d3ad1
Diego5529 <diego.6.souza@gmail.com>
2015-10-08 00:23:41 -0300

    iniciando bootstrap

--- Gemfile
+++ Gemfile
@@ -29,7 +29,9 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'devise'
-
+gem 'bootstrap-sass'
+gem 'autoprefixer-rails'
+gem 'font-awesome-rails'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,17 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require bootstrap
+//= require jquery.datetimepicker
 //= require_tree .
+
+$(document).ready(function(){
+  $("#event_when").datetimepicker({
+    format:'Y/m/d H:i'
+  });
+  $("#user_dob").datetimepicker({
+    timepicker:false,
+    format:'Y/m/d',
+    maxDate:'0'
+  });
+});
