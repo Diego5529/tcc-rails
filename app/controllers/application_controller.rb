4dc18aa7afa73c81d073b6ab1d64263395e84b75
Diego5529 <diego.6.souza@gmail.com>
2016-07-18 15:43:08 -0300

    Criando layout com bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :name, :last_name])
   end
 end
